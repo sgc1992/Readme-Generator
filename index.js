822e7af700083de3e827efd9269c83bb772aa50b
Sandeep GC <sandeep_gc@hotmail.com>
2021-05-07 18:33:12 +1000

    create readme content

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateHTML = (answers) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+const generateReadMe = (answers) =>
+  `## Table of Contents
+- [Title](#title)
+- [Description](#summary)
+- [Installation](#installation)
+- [License](#license)
+- [Tests](#tests)
+- [Username](#username)
+- [Email](#email)
+
+<h1 align="center">${answers.projectTitle}</h1>
+
+## Title
+${answers.Title}
+
+## Summary
+${answers.summary}
+
+## Installation
+${answers.installation}
+
+## License
+${answers.license}
+
+## Tests
+${answers.tests}
+
+## Username
+${answers.username}
+
+## Email
+${answers.email}`;
 
 inquirer
   .prompt([
     {
       type: "input",
-      name: "projectTitle",
+      name: "Title",
       message: "What is the title of the project?",
     },
     {
@@ -73,9 +82,9 @@ inquirer
     },
   ])
   .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+    const generateContent = generateReadme(answers);
 
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
+    fs.writeFile('README.md', generateContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md')
     );
   });
